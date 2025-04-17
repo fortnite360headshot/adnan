9055c05e91a7991212eadb34c3f4c769b6418536
apple <apple@apples-MacBook-Pro.local>
2025-04-17 18:15:09 +0300

    my first project

---
+++ index.js
@@ -0,0 +1,85 @@
+
+//TYPE OF.
+//let x = 100;
+//console.log(`your are ${x} miles away`);
+//console.log(typeof x);
+
+//BOOLEAN.
+//let online = false;
+//console.log(`i am online: ${online} `);
+
+//ALETING.
+ //alert(`bye`);
+
+//VARIABLES.
+//let fullname = "adnan juma";
+//let age = 25;
+//let student = true;
+//console.log(`what is your fullname:${fullname}`);
+
+//BOOLEAN.
+//let online = true;
+//console.log(`i am online:${online}`)`
+                            //wednesday 4 2024.
+//MATHEMATICS OPERATION.
+//let teacher = 12
+//teacher = teacher -3
+//console.log(`${teacher}`);
+
+//ACCEPTING USER INPUT.
+ //let username;
+ //username = window.prompt('enter your username');
+ //console.log(username);
+
+ //CHANGING THE DATATYPE TO NUMBERS,STRINGS,BOOLEANS.
+ //let age = window.prompt("how old are you?")
+ //age+=1
+ //console.log(age, typeof age);
+
+ //CONST = A VARIABLE THAT CAN' BE CHANGED.
+ //let height;
+ //let width = 60;
+// let hypotenuse = 60;
+ //const PERIMETER = 180
+  
+ //height = window.prompt("enter your height");
+  //height = Number(heighyt)  
+ //height = 180 - 60 + 60;
+
+ //console.log(height);
+//MAXIMUM AND MINIMUM
+ //let x = 23;
+ //let v = 7;
+ //let y = 9;
+
+// mwhay = Math.min(x,v,y,);
+
+// console.log(mwhay);
+
+
+
+//let a;
+//let b;
+//let c;
+//document.getElementById(`submit`).onclick = function( ){
+//a = document.getElementById(`side a`).value;
+//a = Number(a);
+
+//b = document.getElementById(`side b`).value;
+//b = Number(b);
+
+
+//c = Math.sqrt(a*a + b*b);
+
+//console.log(`side c = ${c}`);
+
+//}
+
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `Hello ${username}`
+}
+
+
+
